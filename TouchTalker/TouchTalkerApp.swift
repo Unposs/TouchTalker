e1648b30b5661026858f8e0352f81f22895568e0
Haoyu <haoyu@wanghaoyu.net>
2025-10-11 10:33:40 -0700

    Initial Commit

---
+++ TouchTalker/TouchTalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchTalkerApp.swift
+//  TouchTalker
+//
+//  Created by Yong Wang on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchTalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
